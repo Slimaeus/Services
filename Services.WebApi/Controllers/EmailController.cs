20bd3c455048adcbfc6ce0f28fd9463247593c52
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-08 18:39:55 +0700

    Add project files.

---
+++ Services.WebApi/Controllers/EmailController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.WebApi.Services;
+
+namespace Services.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    public EmailService EmailService => HttpContext.RequestServices.GetRequiredService<EmailService>();
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage()
+    {
+        await EmailService.SendEmailAsync("example@gmail.com", "Nice", "This is message");
+        return Ok();
+    }
+
+}
---
+++ Services.WebApi/Services/EmailService.cs
@@ -0,0 +1,46 @@
+﻿namespace Services.WebApi.Services;
+
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+public class EmailService
+{
+    private readonly string _smtpHost;
+    private readonly int _smtpPort;
+    private readonly string _smtpUsername;
+    private readonly string _smtpPassword;
+
+    public EmailService()
+    {
+        _smtpHost = "smtp.gmail.com";
+        _smtpPort = 587;
+        _smtpUsername = "mail";
+        _smtpPassword = "password";
+    }
+
+    public async Task SendEmailAsync(string recipientEmail, string subject, string message)
+    {
+        var smtpClient = new SmtpClient(_smtpHost, _smtpPort)
+        {
+            Host = _smtpHost,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(_smtpUsername, _smtpPassword),
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_smtpUsername),
+            Subject = subject,
+            Body = message,
+            IsBodyHtml = true
+        };
+
+        mailMessage.To.Add(recipientEmail);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
+
