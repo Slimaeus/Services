5549294d1d34990e4fbcadc5fc1c64d34eafa8c7
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-07-04 11:35:07 +0700

    feat: Finish for Consumer and Producer

--- RabbitMQ/Services.RabbitMQ.Consumer/Program.cs
+++ RabbitMQ/Services.RabbitMQ.Consumer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = ConnectionFactory.DefaultUser,
+    Password = ConnectionFactory.DefaultPass,
+    Port = AmqpTcpEndpoint.UseDefaultPort
+};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+while (true)
+{
+    string message = Console.ReadLine() ?? string.Empty;
+
+    if (message == string.Empty) break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
--- RabbitMQ/Services.RabbitMQ.Producer/Program.cs
+++ RabbitMQ/Services.RabbitMQ.Producer/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = ConnectionFactory.DefaultUser,
+    Password = ConnectionFactory.DefaultPass,
+    Port = AmqpTcpEndpoint.UseDefaultPort
+};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
